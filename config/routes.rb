c8b6dfacf72607c3872bbac85a8c62616f0693e8
Fusco - David Demonchy <fusco.spv@gmail.com>
2011-01-11 17:41:35 +0100

    add model post for texting ckeditor

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ end
 
 
 CkeditorFilemanager::Application.routes.draw do
+
+  resources :posts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -57,7 +60,7 @@ CkeditorFilemanager::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "posts#index"
 
   # See how all your routes lay out with "rake routes"
 
